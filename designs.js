bde715edd3e18288fe4da19b843bd3096a0ea197
Sarah Maris <sarah@sarahmaris.com>
2017-09-02 15:46:49 -0400

    Add changeColor function and clean up code

--- designs.js
+++ designs.js
@@ -1,27 +1,39 @@
 'use strict'
 
-const $colorPicker = document.getElementById("colorPicker")
-let colorInput = $colorPicker.value;
+// set global variables
+const $colorPicker = document.getElementById("colorPicker");
 const table = document.getElementById("pixel_canvas");
-  console.log(colorInput)
+let colorInput = $colorPicker.value;
 
-// Listener to select color input
+// add listener to select color input
 $colorPicker.addEventListener('input', function() {
   colorInput = $colorPicker.value;
   console.log(colorInput)
 })
 
-// Function to draw canvas
+// Draw canvas when dimenstions are submitted
 function makeGrid(event) {
+  // prevent page refresh on submit
   event.preventDefault();
-  // Select size input
+
+  // get input data
   let width= document.getElementById("input_width").value,
       height= document.getElementById("input_height").value;
+
+  // draw grid
   for (let row = 0; row < width; row++) {
     let newRow = table.insertRow();
       for (let cell= 0; cell < height; cell++ ) {
+
+        // add new cell with listener to change color
         let newCell = newRow.insertCell();
+        newCell.onclick = changeColor;
       }
   }
 
+}
+
+// change the color of the clicked cell to current color
+function changeColor() {
+  this.style.background = colorInput;
 }
