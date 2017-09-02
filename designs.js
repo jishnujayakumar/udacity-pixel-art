01592d599cfc6e997a320b60334f808dcb7e90bd
Sarah Maris <sarah@sarahmaris.com>
2017-09-02 16:15:45 -0400

    Update to use lister for sizePicker

--- designs.js
+++ designs.js
@@ -2,29 +2,30 @@
 
 // set global variables
 const $colorPicker = document.getElementById("colorPicker");
+const $sizePicker = document.getElementById("sizePicker");
 const table = document.getElementById("pixel_canvas");
 let colorInput = $colorPicker.value;
 
-// add listener to select color input
+// add listeners to select color input and grid size
 $colorPicker.addEventListener('input', function() {
   colorInput = $colorPicker.value;
-  console.log(colorInput)
 })
 
-// Draw canvas when dimenstions are submitted
-function makeGrid(event) {
+$sizePicker.addEventListener('submit', function() {
   // prevent page refresh on submit
   event.preventDefault();
 
-  // get input data
+  // get input data and draw grid
   let width= document.getElementById("input_width").value,
       height= document.getElementById("input_height").value;
+  makeGrid(width, height);
+})
 
-  // draw grid
+// Draw grid
+function makeGrid(width, height) {
   for (let row = 0; row < width; row++) {
     let newRow = table.insertRow();
       for (let cell= 0; cell < height; cell++ ) {
-
         // add new cell with listener to change color
         let newCell = newRow.insertCell();
         newCell.onclick = changeColor;
