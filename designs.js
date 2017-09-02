74bcc9553b05fca4d8b6bc569d61bbf9609844f1
Sarah Maris <sarah@sarahmaris.com>
2017-09-02 15:38:04 -0400

    Add functions to capture color and make grid

--- designs.js
+++ designs.js
@@ -1,10 +1,27 @@
-// Select color input
-// Select size input
+'use strict'
 
-// When size is submitted by the user, call makeGrid()
+const $colorPicker = document.getElementById("colorPicker")
+let colorInput = $colorPicker.value;
+const table = document.getElementById("pixel_canvas");
+  console.log(colorInput)
 
-function makeGrid() {
+// Listener to select color input
+$colorPicker.addEventListener('input', function() {
+  colorInput = $colorPicker.value;
+  console.log(colorInput)
+})
 
-// Your code goes here!
+// Function to draw canvas
+function makeGrid(event) {
+  event.preventDefault();
+  // Select size input
+  let width= document.getElementById("input_width").value,
+      height= document.getElementById("input_height").value;
+  for (let row = 0; row < width; row++) {
+    let newRow = table.insertRow();
+      for (let cell= 0; cell < height; cell++ ) {
+        let newCell = newRow.insertCell();
+      }
+  }
 
 }
